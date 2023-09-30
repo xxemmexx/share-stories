fc8408f0d719dda8daf9da648f89e52912f242c0
xxemmexx <emme@pm.me>
2023-09-30 22:58:00 +0200

    Added users and footnote text to side nav

--- app/layouts/includes/SideNavMain.tsx
+++ app/layouts/includes/SideNavMain.tsx
@@ -24,20 +24,49 @@ export default function SideNavMain() {
                         colorString={pathname == '/' ? '#F02C56' : ''}
                         sizeString="25"
                     />
-
-                    <MenuItem iconString="Following" colorString='#000000' sizeString="25"/>
-                    <MenuItem iconString="LIVE" colorString='#000000' sizeString="25"/>
-
-                    <div className="border-b lg:ml-2 mt-2"/>
-                    <h3 className="lg:block hidden text-xs text-gray-600 font-semibold pt-4 pb-2 px-2">Suggested storytellers</h3>
-
-                    <div className="lg:hidden block pt-3"/>
-                    <ClientOnly>
-                        <div className="cursor-pointer">
-                            <MenuItemFollow user={{id: "1", name: "Test user", image: "https://placeholder.co/50"}} />
-                        </div>
-                    </ClientOnly>
                 </Link>
+
+                <MenuItem iconString="Following" colorString='#000000' sizeString="25"/>
+                <MenuItem iconString="LIVE" colorString='#000000' sizeString="25"/>
+
+                <div className="border-b lg:ml-2 mt-2"/>
+                <h3 className="lg:block hidden text-xs text-gray-600 font-semibold pt-4 pb-2 px-2">Suggested storytellers</h3>
+
+                <div className="lg:hidden block pt-3"/>
+                <ClientOnly>
+                    <div className="cursor-pointer">
+                        <MenuItemFollow user={{id: "1", name: "Test user", image: "https://placeholder.co/50"}} />
+                    </div>
+                </ClientOnly>
+
+                <button className="lg-block hidden text-[#F02C56] pt-1.5 pl-2 text-[13px]">
+                    See all
+                </button>
+
+                {!true ? (
+                    <div>
+                        <div className="border-b lg:ml-2 mt-2"/>
+                        <h3 className="lg:block hidden text-xs text-gray-600 font-semibold pt-4 pb-2 px-2">Following storytellers</h3>
+
+                        <div className="lg:hidden block pt-3"/>
+                        <ClientOnly>
+                            <div className="cursor-pointer">
+                                <MenuItemFollow user={{id: "1", name: "Test user", image: "https://placeholder.co/50"}} />
+                            </div>
+                        </ClientOnly>
+
+                        <button className="lg-block hidden text-[#F02C56] pt-1.5 pl-2 text-[13px]">
+                            See more
+                        </button>   
+                    </div>
+                ) : null}
+
+                <div className="lg:block hidden border-b lg:ml-2 mt-2"/>
+                
+                <div className="lg:block hidden text-[11px] text-gray-500">
+                    <p className="pt-4 px-2"> About Newsroom Storytelling Shop Contact Careers</p>
+                    <p className="pt-4 px-2"> 2023 Share Stories</p>
+                </div>
             </div>
         </div>
     )
