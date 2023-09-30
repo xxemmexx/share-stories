c3f9432c22e550099c9d8fdca523b6b90b1c11c0
xxemmexx <emme@pm.me>
2023-09-30 22:40:48 +0200

    Added MenuItemFollow component

--- app/layouts/MainLayout.tsx
+++ app/layouts/MainLayout.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { usePathname } from 'next/navigation'
 import TopNav from './includes/TopNav';
+import SideNavMain from './includes/SideNavMain';
 
 export default function MainLayout({ children }: { children: React.ReactNode }) {
     const pathname = usePathname()
@@ -9,6 +10,7 @@ export default function MainLayout({ children }: { children: React.ReactNode }) 
         <>
             <TopNav/>
             <div className={`flex justify-between mx-auto w-full lg:px-2.5 px-0 ${pathname == '/' ? 'max-w-[1140px]' : ''}`}>
+                <SideNavMain/>
                 {children}
             </div>
         
---
+++ app/types.tsx
@@ -0,0 +1,17 @@
+// LAYOUT INCLUDE TYPES
+
+export interface RandomUsers {
+    id: string,
+    name: string,
+    image: string
+}
+
+export interface MenuItemsTypes {
+    iconString: string,
+    colorString: string,
+    sizeString: string
+}
+
+export interface MenuItemFollowCompTypes {
+    user: RandomUsers
+}
